ebe74b68123ace12b56f08e04ac7bef12cdb4b59
uronuma K <you11337799@gmail.com>
2021-12-27 21:24:44 +0800

    Test

--- yahoo/index.js
+++ yahoo/index.js
@@ -1,6 +1,8 @@
-const fb_username = process.env.FB_USERNAME;
-const fb_userpass = process.env.FB_PASSWORD;
+//const fb_username = process.env.FB_USERNAME;
+//const fb_userpass = process.env.FB_PASSWORD;
 
+const { initDriver } = require('./tools/initDriver.js')
+const { SearchBook } = require('./tools/SearchBook.js')
 const webdriver = require('selenium-webdriver'), // 加入虛擬網頁套件
     By = webdriver.By, // 你想要透過什麼方式來抓取元件，通常使用xpath、css
     until = webdriver.until; // 直到抓到元件才進入下一步(可設定等待時間)
@@ -14,82 +16,13 @@ options.setUserPreferences({ 'profile.default_content_setting_values.notificatio
 const path = require('path'); // 用於處理文件路徑的小工具
 const fs = require("fs"); // 讀取檔案用
 
-function checkDriver() {
-    try {
-        chrome.getDefaultService(); // 確認是否有預設        
-    } catch {
-        console.warn('找不到預設driver!');
-        const file_path = '../chromedriver.exe'; // '../chromedriver.exe'記得調整成自己的路徑
-        console.log(path.join(__dirname, file_path)); // 請確認印出來日誌中的位置是否與你路徑相同
-        if (fs.existsSync(path.join(__dirname, file_path))) { // 確認路徑下chromedriver.exe是否存在            
-            const service = new chrome.ServiceBuilder(path.join(__dirname, file_path)).build(); // 設定driver路徑
-            chrome.setDefaultService(service);
-            console.log('設定driver路徑');
-        } else {
-            console.error('無法設定driver路徑');
-            return false;
-        }
-    }
-    return true;
-}
-
-async function SearchBook() {
-    let driver;
-    if (!checkDriver()) {// 檢查driver是否設定，如果無法設定就結束程式
+async function SearchBookTest() {
+    
+    if (!initDriver) {// 檢查driver是否設定，如果無法設定就結束程式
         return;
     }
-    try {
-        driver = await new webdriver.Builder().forBrowser("chrome").withCapabilities(options).build(); // 建立這個Browser的類型
-    } catch (e) {
-        console.error('無法建立瀏覽器!');
-        console.error(e);
-        return;
-    }
-    const web = 'https://www.books.com.tw/?loc=tw_website_001'; // FB登入頁面
-    await driver.get(web); // 在這裡要用await確保打開完網頁後才能繼續動作
-    await driver.sleep(1000);
-    //同上
-    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[@class="hot_key_words clearfix"]`)));
-    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[@class="hot_key_words clearfix"]`)));
-    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[contains(@class,"hot_key_words clearfix")]`)));
-    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[contains(@class,"hot_key_words clearfix")]`)));
-    
-    //console.log(booknotifications)
-    //for (const ff of booknotifications)
-    //{
-    //    const fs1 = await ff.getText();
-    //    console.log(fs1);
-    //}
-    
 
-
-    
-    const closeBanner = await driver.wait(until.elementLocated(By.xpath(`//*[@id="close_top_banner"]`)));
-    //成功關閉 廣告 close
-    closeBanner.click();
-    await driver.sleep(200);
-
-    const inputKind = await driver.wait(until.elementLocated(By.xpath(`//*[@name="key"]`)));
-    console.log(inputKind);
-    const stringvalue = "javascript";
-    inputKind.sendKeys(stringvalue);
-    console.log(inputKind);
-    // 無法輸入的原因 sleep 需要時間輸入 done in 8.15s
-    await driver.sleep(300);
-
-    const SearchButton = await driver.wait(until.elementsLocated(By.xpath(`//*button[text()="Submit")]`)));
-    //const SearchButton = await driver.wait(until.elementsLocated(By.xpath(`//*[@id="search"]/button`)));
-    //const SearchButton = driver.wait(By.xpath(`//*[@id="search"]/button`));
-    
-    console.log(SearchButton);
-    SearchButton.click();
-    await driver.sleep(200);
-    
-
-    //await driver.sleep(100);
-
-
-    driver.quit();
+    SearchBook();
 
 }
-SearchBook();
+SearchBookTest();
--- yahoo/tools/SearchBook.js
+++ yahoo/tools/SearchBook.js
@@ -1,45 +1,83 @@
+
+var Ch = require("jsonschema").Validator;
+var vv = new Ch();
+var ins = 4;
+var schema = { "type": "number" };
 //const { request } = require("http");
 
 //載入專案
-//exports.SearchBook = SearchBook;
-//require("dotenv").config();
-////const { initDriver } = require("./tools/initDriver.js"); //載入資料
-////const driver = await initDriver();
-//const { initDriver } = require("./initDriver");
-//const webdriver = require("selenium-webdriver");
-//const { By, until } = require("selenium-webdriver"); //
-//const chrome = require("selenium-webdriver/chrome"); //chrome 載入設定
-//
-//const pathRoute = process.env.PathRoute;
-//
-//const path = require("path");
-//const fs = require("fs");
-//
-//async function SearchBook() { 
-//    //console.log(pathRoute);
-//    if (!initDriver)
-//    {
-//        return;
-//    }
-//    let driver;
-//    try {
-//       
-//        driver = await new webdriver.Builder().forBrowser("chrome").build();
-//
-//        //.withCapabilities(options);
-//         //driver = await new webdriver.Builder().forBrowser("chrome").withCapabilities(options).build(); // 建立這個Browser的類型
-//        //await driver.manage().window();
-//        //chrome.setDefaultService('https://www.books.com.tw/?loc=tw_logo_001');
-//        //driver.get(pathRoute);
-//        //console.log(pathRoute);
-//        //return driver;
-//    } catch (e)
-//    {
-//        //console.error(e);
-//        console.error('無法建立瀏覽器!');
-//        console.error(e);
-//        return;
-//    }
-//    await driver.get(pathRoute); // 開啟網頁
-//
-//}
+exports.SearchBook = SearchBook;
+require("dotenv").config();
+const webdriver = require("selenium-webdriver");
+const { By, until } = require("selenium-webdriver"); //
+const chrome = require("selenium-webdriver/chrome"); //chrome 載入設定
+
+
+const options = new chrome.Options();
+options.addArguments('--log-level=3'); // 這個option可以讓你跟網頁端的console.log說掰掰
+// 因為notifications會干擾到爬蟲，所以要先把它關閉
+options.setUserPreferences({ 'profile.default_content_setting_values.notifications': 1 });
+const pathRoute = process.env.PathRoute;
+const path = require("path");
+const fs = require("fs");
+
+async function SearchBook() { 
+
+    let e = vv.validate(ins, schema);
+    console.log(e);
+
+    let driver;
+    try {
+        driver = await new webdriver.Builder().forBrowser("chrome").withCapabilities(options).build(); // 建立這個Browser的類型
+    } catch (e) {
+        console.error('無法建立瀏覽器!');
+        console.error(e);
+        return;
+    }
+    const web = 'https://www.books.com.tw/?loc=tw_website_001'; // FB登入頁面
+    await driver.get(web); // 在這裡要用await確保打開完網頁後才能繼續動作
+    await driver.sleep(1000);
+    //同上
+    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[@class="hot_key_words clearfix"]`)));
+    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[@class="hot_key_words clearfix"]`)));
+    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[contains(@class,"hot_key_words clearfix")]`)));
+    //const booknotifications = await driver.wait(until.elementsLocated(By.xpath(`//*[contains(@class,"hot_key_words clearfix")]`)));
+
+    //console.log(booknotifications)
+    //for (const ff of booknotifications)
+    //{
+    //    const fs1 = await ff.getText();
+    //    console.log(fs1);
+    //}
+
+
+
+
+    const closeBanner = await driver.wait(until.elementLocated(By.xpath(`//*[@id="close_top_banner"]`)));
+    //成功關閉 廣告 close
+    closeBanner.click();
+    await driver.sleep(200);
+
+    const inputKind = await driver.wait(until.elementLocated(By.xpath(`//*[@name="key"]`)));
+    //console.log(inputKind);
+    const stringvalue = "javascript";
+    inputKind.sendKeys(stringvalue);
+    //console.log(inputKind);
+    // 無法輸入的原因 sleep 需要時間輸入 done in 8.15s
+    await driver.sleep(300);
+
+    const SearchButton = await driver.wait(until.elementLocated(By.xpath(`//*[@id="search"]/button`)));
+    //const SearchButton = await driver.wait(until.elementsLocated(By.xpath(`//*[@id="search"]/button`)));
+    //const SearchButton = driver.wait(By.xpath(`//*[@id="search"]/button`));
+
+    //console.log(SearchButton);
+    SearchButton.click();
+    await driver.sleep(200);
+
+
+    //await driver.sleep(100);
+
+
+    driver.quit();
+
+}
